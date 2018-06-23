3396b88cc7d5fd6f94af7880636543d834b04505
johnsong <shocking82@nate.com>
2018-06-23 19:59:38 +0900

    Initial commit

---
+++ src/main/java/com/johnsong/android/spring/Post.java
@@ -0,0 +1,49 @@
+package com.johnsong.android.spring;
+
+public class Post {
+    private long id;
+    private String title;
+    private String content;
+    private String created_at;
+    private String update_at;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(String update_at) {
+        this.update_at = update_at;
+    }
+}
